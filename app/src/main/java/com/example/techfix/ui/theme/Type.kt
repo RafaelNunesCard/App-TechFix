18a8772ed279ccf1be8758172f85d55c78b3ed83
João Victor <joaovictorbetanzo@gmail.com>
2026-09-13 19:39:17 -0300

    Adiciona tela de serviços com fonte Sora e perfis

--- app/src/main/java/com/example/techfix/ui/theme/Type.kt
+++ app/src/main/java/com/example/techfix/ui/theme/Type.kt
@@ -2,11 +2,17 @@ package com.example.techfix.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.techfix.R
 
 // Set of Material typography styles to start with
+val SoraFamily = FontFamily(
+    Font(R.font.sora_regular, FontWeight.Normal),
+    Font(R.font.sora_bold, FontWeight.Bold)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
